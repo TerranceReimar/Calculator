6bd4683b85c68a403f4c05361b1428d5cea29d4f
TerranceReimar <terrancereimar@gmail.com>
2023-01-03 14:16:34 +0200

    Refactor

--- script.js
+++ script.js
@@ -1,164 +1,84 @@
 const screen = document.querySelector(".screen");
-
-// OUTPUT FORMULA ON SCREEN ELEMENT //
-const displayFormula = document.createElement("div");
+let displayFormula = document.createElement("div");
+let displayUser = document.createElement("div");
 displayFormula.classList.add("displayFormula");
-
-// OUTPUT CURRENT INPUT ON SCREEN ELEMENT //
-const displayIO = document.createElement("div");
-displayIO.classList.add("displayIO");
-
+displayUser.classList.add("displayUser");
 screen.appendChild(displayFormula);
-screen.appendChild(displayIO);
+screen.appendChild(displayUser);
 
-let userInput;
+let userInput = [];
 let formula = [];
 
 const clear = document.querySelector("#clear");
 clear.addEventListener("click", () => {
-  userInput = undefined;
-  formula = undefined;
-  displayIO.textContent = "";
+  userInput = [];
+  formula = [];
+  displayFormula.textContent = "";
+  displayUser.textContent = "";
 });
 
 const del = document.querySelector("#delete");
 del.addEventListener("click", () => {
-  if (userInput !== undefined) {
-    userInput = Array.from(userInput);
-    userInput.pop();
-    userInput = userInput.join("");
-  }
-  displayIO.textContent = userInput;
+  userInput.pop();
+  displayUser.textContent = userInput.join("");
 });
 
 const one = document.querySelector("#one");
 one.addEventListener("click", () => {
-  if (userInput !== undefined) {
-    userInput = Array.from(userInput);
-    userInput.push("1");
-    userInput = userInput.join("");
-  } else {
-    userInput = "1";
-  }
-  displayIO.textContent = userInput;
+  userInput.push("1");
+  displayUser.textContent = userInput.join("");
 });
 
 const two = document.querySelector("#two");
 two.addEventListener("click", () => {
-  if (userInput !== undefined) {
-    userInput = Array.from(userInput);
-    userInput.push("2");
-    userInput = userInput.join("");
-  } else {
-    userInput = "2";
-  }
-  displayIO.textContent = userInput;
+  userInput.push(2);
+  displayUser.textContent = userInput.join("");
 });
 
 const three = document.querySelector("#three");
 three.addEventListener("click", () => {
-  if (userInput !== undefined) {
-    userInput = Array.from(userInput);
-    userInput.push("3");
-    userInput = userInput.join("");
-  } else {
-    userInput = "3";
-  }
-  displayIO.textContent = userInput;
+  userInput.push(3);
+  displayUser.textContent = userInput.join("");
 });
 
 const four = document.querySelector("#four");
 four.addEventListener("click", () => {
-  if (userInput !== undefined) {
-    userInput = Array.from(userInput);
-    userInput.push("4");
-    userInput = userInput.join("");
-  } else {
-    userInput = "4";
-  }
-  displayIO.textContent = userInput;
+  userInput.push(4);
+  displayUser.textContent = userInput.join("");
 });
 
 const five = document.querySelector("#five");
 five.addEventListener("click", () => {
-  if (userInput !== undefined) {
-    userInput = Array.from(userInput);
-    userInput.push("5");
-    userInput = userInput.join("");
-  } else {
-    userInput = "5";
-  }
-  displayIO.textContent = userInput;
+  userInput.push(5);
+  displayUser.textContent = userInput.join("");
 });
 
 const six = document.querySelector("#six");
 six.addEventListener("click", () => {
-  if (userInput !== undefined) {
-    userInput = Array.from(userInput);
-    userInput.push("6");
-    userInput = userInput.join("");
-  } else {
-    userInput = "6";
-  }
-  displayIO.textContent = userInput;
+  userInput.push(6);
+  displayUser.textContent = userInput.join("");
 });
 
 const seven = document.querySelector("#seven");
 seven.addEventListener("click", () => {
-  if (userInput !== undefined) {
-    userInput = Array.from(userInput);
-    userInput.push("7");
-    userInput = userInput.join("");
-  } else {
-    userInput = "7";
-  }
-  displayIO.textContent = userInput;
+  userInput.push(7);
+  displayUser.textContent = userInput.join("");
 });
 
 const eight = document.querySelector("#eight");
 eight.addEventListener("click", () => {
-  if (userInput !== undefined) {
-    userInput = Array.from(userInput);
-    userInput.push("8");
-    userInput = userInput.join("");
-  } else {
-    userInput = "8";
-  }
-  displayIO.textContent = userInput;
+  userInput.push(8);
+  displayUser.textContent = userInput.join("");
 });
 
 const nine = document.querySelector("#nine");
 nine.addEventListener("click", () => {
-  if (userInput !== undefined) {
-    userInput = Array.from(userInput);
-    userInput.push("9");
-    userInput = userInput.join("");
-  } else {
-    userInput = "9";
-  }
-  displayIO.textContent = userInput;
+  userInput.push(9);
+  displayUser.textContent = userInput.join("");
 });
 
 const zero = document.querySelector("#zero");
 zero.addEventListener("click", () => {
-  if (userInput !== undefined) {
-    userInput = Array.from(userInput);
-    userInput.push("0");
-    userInput = userInput.join("");
-  } else {
-    userInput = "0";
-  }
-  displayIO.textContent = userInput;
-});
-
-const times = document.querySelector("#times");
-times.addEventListener("click", () => {
-  if (formula.length < 1 && userInput !== undefined) {
-    formula.push(userInput);
-    displayFormula.textContent = `${formula[0]} x`;
-    userInput = 0;
-    displayIO.textContent = "";
-  } else if (formula.length >= 1 && userInput !== undefined) {
-    for (let i = 0; i < formula.length; i++) {}
-  }
+  userInput.push(0);
+  displayUser.textContent = userInput.join("");
 });
